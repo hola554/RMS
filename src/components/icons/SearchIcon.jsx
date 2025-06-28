35c1c5b9e144a575a90f501d8d28737f48bc6da5
Lawal Baseet <lawalbaseet2020@gmail.com>
2025-06-28 13:09:36 +0100

    completed navbar

---
+++ src/components/icons/SearchIcon.jsx
@@ -0,0 +1,11 @@
+
+export const SearchIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+            <g opacity="0.6">
+                <circle cx="11.4526" cy="12.2568" r="8.66126" stroke="#61656D" strokeWidth="1.44538" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M17.4767 18.7308L20.8724 22.1177" stroke="#61656D" strokeWidth="1.44538" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+        </svg>
+    )
+}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
+import { NotificationIcon } from './icons/NotificationIcon'
+import { SearchIcon } from './icons/SearchIcon'
 
 export const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between bg-red-300 mx-2 my-3 px-4 h-[80px] rounded-md'>
+    <nav className='flex items-center justify-between bg-white mx-2 my-3 px-4 h-[80px] rounded-md'>
       {/* User name */}
       <span className='text-lg flex-[1]'>
         Welcome back, Prof. <strong>Ogirima</strong>
       </span>
       {/* Search Input */}
-      <div className="flex-[1]">
+      <div className="flex-[1] relative">
+        <span className='absolute top-2 left-2'>
+          <SearchIcon />
+        </span>
         <input 
-          className='w-full p-2 rounded-md outline-none placeholder:text-sm'
+          className='w-full p-2 pl-10 bg-gray-100 rounded-md outline-none placeholder:text-sm'
           type="Search" 
           placeholder='Search'
         />
       </div>
-      <div className="flex-[1] flex items-center justify-end">
+      <div className="flex-[1] flex gap-4 items-center justify-end">
         {/* status */}
-        <span>online</span>
+        <span className='text-green-700 capitalize'>online</span>
+        <NotificationIcon />
+        <img src="/64.jpg" alt="" />
       </div>
     </nav>
   )
