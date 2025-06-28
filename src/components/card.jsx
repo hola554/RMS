ef8c8d70d38ada7bbb15521c08a6beb609292526
Salami Azeez <salamitopeazeez@gmail.com>
2025-06-28 15:10:46 +0100

    Quick card layout

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
 
 export const Card = () => {
   return (
-    <div>card</div>
+    <div className="bg-[#7542ff]">
+        {/* icon */}
+
+        {/* Details */}
+        <span>Registered students</span>
+        <p>300</p>
+        <Link to="">View details</Link>
+    </div>
   )
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,18 @@
-
+import { Card } from "../components/card";
 
 const DashboardPage = () => {
- 
-    return (
-        <>
-            <main className="">
-                dashboard
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <main className="px-3 bg-red-400 w-full">
+        <section className="flex gap-2 w-full">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
