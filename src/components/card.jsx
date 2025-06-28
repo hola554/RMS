3d8fbfe77bb586cbfab11fbec18435099801194d
Lawal Baseet <lawalbaseet2020@gmail.com>
2025-06-28 15:31:17 +0100

    Completed layout for card section for dashboard except icon

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 
-export const Card = () => {
+export const Card = ({ title, stat, bgColor }) => {
   return (
-    <div className="bg-[#7542ff]">
+    <div
+      style={{
+        background: bgColor
+      }}
+      className=" text-white py-10 px-5 rounded-lg flex-[1]">
         {/* icon */}
 
         {/* Details */}
-        <span>Registered students</span>
-        <p>300</p>
-        <Link to="">View details</Link>
-    </div>
+        <span className="text-[15px] capitalize">{title}</span>
+        <p className="text-3xl font-bold">{stat}</p>
+        <Link to="" className="text-[13px] underline">View details</Link>
+    </div> 
   )
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,12 +3,21 @@ import { Card } from "../components/card";
 const DashboardPage = () => {
   return (
     <>
-      <main className="px-3 bg-red-400 w-full">
+      <main className="px-3 w-full">
+        {/* card section */}
         <section className="flex gap-2 w-full">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card title="registered students"
+            stat="300"
+            bgColor="#7542ff" />
+          <Card title="total staffs"
+            stat="38"
+            bgColor="#31A093" />
+          <Card title="results submitted"
+            stat="285"
+            bgColor="#FE9A62" />
+          <Card title="pending registration"
+            stat="50"
+            bgColor="#7CBE3B" />
         </section>
       </main>
     </>
